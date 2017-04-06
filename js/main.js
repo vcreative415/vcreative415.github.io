f16f8e124c774d587e1fefee0938c9a2614fc2de
vcreative415 <vanessa@rallonza.com>
2017-04-05 20:51:59 -0700

    Auto refresh java
    Better script for refreshing page on load

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 // Make it rain!
 
+$(document).ready(function(){    
+    //Check if the current URL contains '#'
+    if(document.URL.indexOf("#")==-1){
+        // Set the URL to whatever it was plus "#".
+        url = document.URL+"#";
+        location = "#";
 
+        //Reload the page
+        location.reload(true);
+    }
+});
 
 // Smooth scroll this mother!
 
